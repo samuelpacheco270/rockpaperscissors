ca8992236dde8fd5fa8de8692a2c747c5f900ad2
samuelpacheco270 <sp6236@thewcs.org>
2019-11-21 22:56:34 +0000

    finish project

--- js/script.js
+++ js/script.js
@@ -13,4 +13,43 @@
 $("#shoot").click( function() {
     let input = $("#input").val();
     $("#userChoice").html(`<p> ${input}</p>`);
+
+let computerChoice = Math.random()
+if (computerChoice < 1/3) {
+    $("#computerChoice").html(`<p>Scissors</p>`);
+    if (input === 'paper') {
+        $("#result").html("<p>Computer wins</P>")
+    }
+    if (input === 'rock') {
+        $("#result").html("<p>You win</P>")
+    }
+    if (input === 'scissors') {
+        $("#result").html("<p>Tie</P>")
+    }
+}
+else if (computerChoice > 1/3 && computerChoice < 2/3){
+       $("#computerChoice").html(`<p>Paper</p>`);
+        if (input === 'paper') {
+        $("#result").html("<p>Tie</P>")
+    }
+    if (input === 'rock') {
+        $("#result").html("<p>Computer wins</P>")
+    }
+    if (input === 'scissors') {
+        $("#result").html("<p>You win</P>")
+    }
+}
+else if (computerChoice > 2/3){
+       $("#computerChoice").html(`<p>Rock</p>`);
+        if (input === 'paper') {
+        $("#result").html("<p>You win</P>")
+    }
+    if (input === 'rock') {
+        $("#result").html("<p>Tie</P>")
+    }
+    if (input === 'scissors') {
+        $("#result").html("<p>Computer wins</P>")
+    }
+}
+
 } );
